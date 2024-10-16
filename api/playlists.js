0fdf16276d26766cdd7edfdffffd3295c533ae49
beefchief <c.lang.1993@gmail.com>
2024-10-15 21:52:29 -0500

    homework complete, all reqs function

--- api/playlists.js
+++ api/playlists.js
@@ -1,3 +1,42 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const playlists = await prisma.playlist.findMany();
+    res.json(playlists);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, description, ownerId, trackIds } = req.body;
+  const tracks = trackIds.map((id) => ({ id: +id }));
+  try {
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        ownerId: +ownerId,
+        tracks: { connect: tracks },
+      },
+      include: { owner: true, tracks: true },
+    });
+    res.status(201).json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const playlist = await prisma.playlist.findUnique({ where: { id: +id } });
+    res.json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
--- api/users.js
+++ api/users.js
@@ -1,3 +1,23 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({ where: { id: +id } });
+    res.json(user);
+  } catch (e) {
+    next(e);
+  }
+});
