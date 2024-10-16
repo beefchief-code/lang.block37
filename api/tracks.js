9297371af41351c669db14c731ef29a2cbffb2a7
beefchief <c.lang.1993@gmail.com>
2024-10-15 21:30:27 -0500

    tracks routes complete

---
+++ api/tracks.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tracks = await prisma.track.findMany();
+    res.json(tracks);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const track = await prisma.track.findUnique({ where: { id: +id } });
+    res.json(track);
+  } catch (e) {
+    next(e);
+  }
+});
